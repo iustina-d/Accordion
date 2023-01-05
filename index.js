1c463ef71d06b7212d07d13ba816d769790944bf
iustina-d <iustina2dulea@gmail.com>
2023-01-05 16:11:31 +0200

    make the answer disappear when click on another question

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const questions = document.querySelectorAll(".question")
 questions.forEach((question) => {
   question.addEventListener("click", () => {
-    question.classList.toggle("open")
+    if (question.classList.contains("open")) {
+      question.classList.remove("open")
+    } else {
+      const questionsIsOpen = document.querySelectorAll(".question.open")
+      questionsIsOpen.forEach((quest) => {
+        quest.classList.remove("open")
+      })
+      question.classList.add("open")
+    }
   })
 })
